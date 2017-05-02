f1c79313075c98fe3270225d357b90b4247aab17
dw <347100466@qq.com>
2017-05-02 11:26:48 +0800

    查看座位功能

--- manage/cinemaManage/AddElement.js
+++ manage/cinemaManage/AddElement.js
@@ -1,14 +1,34 @@
 import React from "react";
 import {ajax} from "../../tool/tools";
 import 'antd/dist/antd.css';
+import Seat from "./Seat";
 import {Modal,Button,Input,Form,Icon,message} from 'antd';
 const FormItem = Form.Item;
 
+let uuid = 0;
 class AddButton extends React.Component{
   constructor(props){
     super(props);
-    this.state = { visible: false } 
+    this.state = { 
+          visible: false ,
+          seatvisible: false ,
+      } 
   }
+
+  showSeats(k){
+      let value = this.props.form.getFieldValue(`seats-${k}`);
+      console.log(value);
+      this.setState({
+          seatvisible:true,
+          seats:value
+      });
+  }
+  SeathandleOk(){
+      this.setState({ 
+        seatvisible: false,
+      });  
+    }
+
   handleSubmit(e){
       e.preventDefault();
       this.props.form.validateFields((err, values) => {
@@ -41,8 +61,35 @@ class AddButton extends React.Component{
   handleChange(value) {
       console.log(`selected ${value}`);
   }
+  remove(k){
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  }
+
+  add(){
+      uuid++;
+      const { form } = this.props;
+      // can use data-binding to get
+      const keys = form.getFieldValue('keys');
+      const nextKeys = keys.concat(uuid);
+      // can use data-binding to set
+      // important! notify form to detect changes
+      form.setFieldsValue({
+        keys: nextKeys,
+      });
+  }
   render(){
-      const { getFieldDecorator } = this.props.form;
+      const { getFieldDecorator,getFieldValue} = this.props.form;
       const formItemLayout = {
           labelCol: {
             xs: { span: 24 },
@@ -53,8 +100,73 @@ class AddButton extends React.Component{
             sm: { span: 14 },
           },
       };
+      const formItemLayoutWithOutLabel = {
+        wrapperCol: {
+          xs: { span: 24, offset: 0 },
+          sm: { span: 20, offset: 4 },
+        },
+      };
+      getFieldDecorator('keys', { initialValue: [] });
+      const keys = getFieldValue('keys');
+      const formItems = keys.map((k, index) => {
+        return (
+          <div>
+          <FormItem
+            {...formItemLayout}
+            label='放映厅名'
+            required={false}
+            key={k} 
+          >
+            {getFieldDecorator(`names-${k}`, {
+              validateTrigger: ['onChange', 'onBlur'],
+              rules: [{
+                required: true,
+                whitespace: true,
+                message: "请输入放映厅名.",
+              }],
+            })(
+              <Input type="text" placeholder="放映厅名" style={{ width: '60%', marginRight: 8 }} />
+            )}
+            <Icon
+              className="dynamic-delete-button"
+              type="minus-circle-o"
+              disabled={keys.length === 1}
+              onClick={() => this.remove(k)}
+            />
+          </FormItem>
+
+          <FormItem
+            {... formItemLayout}
+            label='座位'
+            required={false} 
+          >
+            {getFieldDecorator(`seats-${k}`, {
+              validateTrigger: ['onChange', 'onBlur'],
+              rules: [{
+                required: true,
+                whitespace: true,
+                message: "请输入座位.",
+              }],
+            })(
+              <Input type="textarea"  placeholder="座位" style={{ width: '60%', marginRight: 8 }} />
+            )}
+            <Icon
+              className="dynamic-delete-button"
+              type="eye-o"
+              disabled={keys.length === 1}
+              onClick={() => this.showSeats(k)}
+            />
+          </FormItem>
+          </div>
+        );
+      });
       return (
         <span>
+              <Seat seats={this.state.seats}
+                    visible={this.state.seatvisible} 
+                    handleOk={this.SeathandleOk.bind(this)} 
+                    >
+              </Seat>
             <Button type="primary" style={{margin:"10px"}} onClick={this.showModal.bind(this)}>增加</Button>
             <Modal title="添加数据" visible={this.state.visible}
               onOk={this.handleOk.bind(this)} onCancel={this.handleCancel.bind(this)}
@@ -88,6 +200,13 @@ class AddButton extends React.Component{
                       })(
                         <Input type="text"  placeholder="联系电话" />
                       )}
+              </FormItem>
+
+              {formItems}
+              <FormItem {...formItemLayoutWithOutLabel} >
+                <Button type="dashed" onClick={this.add.bind(this)} style={{ width: '60%' }}>
+                  <Icon type="plus" /> 添加放映厅
+                </Button>
               </FormItem>
             </Form>
           </Modal>
---
+++ manage/cinemaManage/Seat.js
@@ -0,0 +1,45 @@
+import React from "react";
+import 'antd/dist/antd.css';
+import './css/style.css';
+import {Modal,Button,Input,Form} from 'antd';
+
+
+export default class Seat extends React.Component{
+	constructor(props){
+    	super(props);
+  	}
+
+  	render(){
+  		let seatValue = this.props.seats || "[]";
+  		seatValue = JSON.parse(seatValue);
+  		let trAry = [];
+  		for(let i = 0;i < seatValue.length;i++){
+            let tdAry = [];
+            for(let j = 0;j < seatValue[i].length;j++){
+                if(seatValue[i][j] == 1){
+                    tdAry.push(<td><i className="seat_optional"></i></td>);
+                }else{
+                    tdAry.push(<td><i className="seat_null">&nbsp;</i></td>);
+                }
+
+            }
+            trAry.push(<tr>{tdAry}</tr>);
+        }
+  		return (
+
+  			<div>
+  				<Modal title="座位" visible={this.props.visible}
+              		onOk={this.props.handleOk} onCancel={this.props.handleOk}
+              		okText="确定" cancelText="取消"
+              		style={{textAlign:"center"}}
+            	>
+            		<table>
+            			<tbody>{trAry}</tbody>
+            		</table>
+            	</Modal>
+  			</div>
+
+
+  			)
+  	}
+}
